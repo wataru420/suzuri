4f9452bccca108c0fc3f146bb8400bf57924a7ca
Wataru Fukunaga <fukunaga_wataru@cyberagent.co.jp>
2014-02-21 10:35:14 +0900

    First Commit

---
+++ app/js/index.js
@@ -0,0 +1,161 @@
+/**
+ * Created by jweber on 01.10.13.
+ */
+var marked = require('marked');
+var gui = require("nw.gui");
+var fs = require("fs");
+var path = require("path");
+
+var cm;
+var vm;
+
+onload = function() {
+
+  cm = CodeMirror.fromTextArea(
+      document.getElementById("markdown_code"),
+      {
+        mode: "markdown",
+        lineNumbers: true
+      });
+
+    cm.on("change", function(cm, change) {
+      cm.save();
+      $("#markdown_content").html(marked($("#markdown_code").val()));
+    });
+
+
+  initContextMenu();
+  newFile();
+
+  $("#saveFile").change(function(evt) {
+    onChosenFileToSave($(this).val());
+  });
+  $("#openFile").change(function(evt) {
+    onChosenFileToOpen($(this).val());
+  });
+
+
+function AppViewModel() {
+    var self = this;
+
+    self.filelist = ko.observableArray([]);
+
+    self.addFile = function() {
+        handleOpenButton()
+        self.filelist.push({ name: "New at " + new Date() });
+    };
+
+    self.removeFile = function() {
+        self.filelist.remove(this);
+    }
+}
+
+vm = new AppViewModel();
+ko.applyBindings(vm);
+
+};
+
+function initContextMenu() {
+var menubar = new gui.Menu({ type: 'menubar' });
+
+var subMenu = new gui.Menu();
+
+var subMenuItem1 = new gui.MenuItem({
+	label: 'NewFile',
+    click: function() {
+      handleNewButton();
+    }
+});
+var subMenuItem2 = new gui.MenuItem({
+	label: 'Open',
+    click: function() {
+      handleOpenButton()
+    }
+});
+var subMenuItem3 = new gui.MenuItem({
+	label: 'Save',
+    click: function() {
+      handleSaveButton()
+    }
+});
+subMenu.append(subMenuItem1);
+subMenu.append(subMenuItem2);
+subMenu.append(subMenuItem3);
+
+menubar.append(new gui.MenuItem({
+	label: 'File',
+	submenu: subMenu
+}));
+
+gui.Window.get().menu = menubar;}
+
+function newFile() {
+  fileEntry = null;
+  hasWriteAccess = false;
+}
+
+function setFile(theFileEntry, isWritable) {
+  fileEntry = theFileEntry;
+  hasWriteAccess = isWritable;
+}
+
+function readFileIntoEditor(theFileEntry) {
+  fs.readFile(theFileEntry, function (err, data) {
+    if (err) {
+      console.log("Read failed: " + err);
+    }
+
+    cm.setValue(String(data));
+    vm.filelist.push({
+        name: path.basename(theFileEntry),
+        path: theFileEntry,
+        content: String(data)
+    });
+
+  });
+}
+
+function writeEditorToFile(theFileEntry) {
+  var str = cm.getValue();
+  fs.writeFile(theFileEntry, cm.getValue(), function (err) {
+    if (err) {
+      console.log("Write failed: " + err);
+      return;
+    }
+
+    console.log("Write completed.");
+  });
+}
+
+var onChosenFileToOpen = function(theFileEntry) {
+  setFile(theFileEntry, false);
+  readFileIntoEditor(theFileEntry);
+};
+
+var onChosenFileToSave = function(theFileEntry) {
+  setFile(theFileEntry, true);
+  writeEditorToFile(theFileEntry);
+};
+
+function handleNewButton() {
+  if (true) {
+    newFile();
+    cm.setValue("");
+  } else {
+    var x = window.screenX + 10;
+    var y = window.screenY + 10;
+    window.open('main.html', '_blank', 'screenX=' + x + ',screenY=' + y);
+  }
+}
+
+function handleOpenButton() {
+  $("#openFile").trigger("click");
+}
+
+function handleSaveButton() {
+  if (fileEntry && hasWriteAccess) {
+    writeEditorToFile(fileEntry);
+  } else {
+    $("#saveFile").trigger("click");
+  }
+}
